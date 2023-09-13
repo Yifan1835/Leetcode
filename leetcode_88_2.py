6925153fc7d9ca33862830fe1d0fd9b3d5f80878
Yifan1835 <103873732+Yifan1835@users.noreply.github.com>
2023-09-13 12:06:10 +0100

    Create leetcode_88_2.py

---
+++ leetcode_88_2.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # Use pointers
+        # Time = O(m+n)
+        # Space = O(1)
+
+        i = m-1 # index of nums1
+        j = n-1 # index of nums2
+        k = m+n-1 # index of nums1+nums2
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k]=nums2[j]
+                k -= 1
+                j -= 1
