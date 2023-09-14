1da4d1b84f9f646315f2555edd48f8dfe70248f8
Yifan1835 <103873732+Yifan1835@users.noreply.github.com>
2023-09-14 11:40:25 +0100

    Create leetcode_0027_2.py

---
+++ leetcode_0027_2.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+
+        k = len(nums)
+        cnt = 0
+        for i in range(k):
+            if nums[i] != val:
+                nums[cnt] = nums[i]
+                cnt += 1
+   
+        return cnt
